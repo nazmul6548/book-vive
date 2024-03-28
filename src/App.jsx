aedc9342c1e707ff2a046e36c070afd12bf107ac
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-03-28 15:12:13 +0600

    final without sort

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
   
 
   return (
-    <div>
-      <Home></Home>
-    </div>
+    // <div>
+    //   <Home></Home>
+    // </div>
   )
 }
 
--- src/pages/Readbook.jsx
+++ src/pages/Readbook.jsx
@@ -3,17 +3,26 @@ import { getBook, saveBook } from "../Utils"
 import Readcart from "../components/Readcart"
 
 
+
 function Readbook() {
     
     const [read,setRead] = useState([])
     useEffect(() => {
 const storebook = getBook()
 setRead(storebook)
+const sortedBooks = storebook.sort((a, b) => b.rating - a.rating);
+    setRead(sortedBooks);
 },[])
+
+
+
+
+
 console.log(read);
    
   return (
     <div>
+      
       {
         read.map((book) => <Readcart key={book.bookId}  book={book}></Readcart>)
       }
